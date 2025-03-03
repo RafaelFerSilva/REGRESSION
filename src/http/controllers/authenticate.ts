e72c9ca715ed8d84521ad5e65a0ffdfcddbfc098
rafaelsilva <todosomtodavoz@gmail.com>
2025-03-03 11:12:57 -0300

    feat: implement register and authenticate factory pattern

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,6 +1,8 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { AuthenticateUseCase } from "@/use-cases/authenticate";
 import { InvalidCredentialsError } from "@/use-cases/erros/invalid-creadentials-error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
@@ -13,8 +15,7 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
   const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+    const authenticateUseCase  = makeAuthenticateUseCase()
     await authenticateUseCase.execute({ email, password })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) return reply.status(400).send({ message: error.message })
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,6 +3,7 @@ import { z } from "zod"
 import { RegisterUseCase } from "@/use-cases/register"
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
 import { UserAlreadyExistError } from "@/use-cases/erros/user-already-exists-error"
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case"
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -15,8 +16,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password, rule} = registerBodySchema.parse(request.body)
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const registerUseCase = makeRegisterUseCase()
     await registerUseCase.execute({
       name,
       email,
