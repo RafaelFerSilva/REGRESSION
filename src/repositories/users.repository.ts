8a1749736d384b7b8cee7501804a439948b33c87
rafaelsilva <todosomtodavoz@gmail.com>
2025-03-03 20:39:57 -0300

    refact: update user repository return

--- src/repositories/users.repository.ts
+++ src/repositories/users.repository.ts
@@ -1,8 +1,7 @@
-import { UserResponse } from "@/interfaces/user.interfaces";
 import { Prisma, User } from "@prisma/client";
 
 export interface UsersRepository {
   create(data: Prisma.UserCreateInput): Promise<User>
-  findbyEmail(email: string): Promise<UserResponse | null>
-  findById(id: string): Promise<UserResponse | null>
+  findbyEmail(email: string): Promise<User | null>
+  findById(id: string): Promise<User | null>
 }
