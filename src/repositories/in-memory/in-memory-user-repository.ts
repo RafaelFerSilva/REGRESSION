1afc850c515ef2bacc2e33b68980a380d5107f3f
rafaelsilva <todosomtodavoz@gmail.com>
2025-03-03 11:32:04 -0300

    feat: Get user profile

--- src/repositories/in-memory/in-memory-user-repository.ts
+++ src/repositories/in-memory/in-memory-user-repository.ts
@@ -2,6 +2,7 @@ import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../users.repository";
 
 export class InMemoryUsersRepository implements UsersRepository {
+  
   public items: User[] = []
 
   async create(data: Prisma.UserCreateInput) {
@@ -23,6 +24,16 @@ export class InMemoryUsersRepository implements UsersRepository {
     const user = this.items.find((item) => item.email == email)
 
     if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id == id)
+
+    if(!user) {
       return null
     }
 
---
+++ src/use-cases/get-user-profile.spect.ts
@@ -0,0 +1,42 @@
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-user-repository"
+import { GetUserProfileUseCase } from "./get-user-profile"
+import { beforeEach, describe, expect } from "vitest"
+import { it } from "node:test"
+import { hash } from "bcryptjs"
+import { ResourceNotFoundError } from "./erros/resource-not-found-error"
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+      rule: 'QA',
+      active: false
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual(createdUser.name)
+  })
+
+  it('should not be able to get user profile with wrong id', async () => {
+    await expect(() =>
+      sut.execute({
+        userId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
+
