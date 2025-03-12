b798c41d4bce78b6e5f2ea42bfd284ac0033d972
rafaelsilva <todosomtodavoz@gmail.com>
2025-03-12 19:40:08 -0300

    test: should be able to get user profile

--- src/http/controllers/Authenticate/autheticate.spec.ts
+++ src/http/controllers/Authenticate/autheticate.spec.ts
@@ -1,3 +1,32 @@
-import { test } from 'vitest'
+import { beforeAll, afterAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', async () => {})
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate user', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+      rule: 'QA',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(authResponse.statusCode).toEqual(200)
+    expect(authResponse.body).toEqual({
+      token: expect.any(String),
+    })  
+  })
+})
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -17,13 +17,14 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.get('/user/:userId', getUserProfile)
   app.get('/users/:page', getUsersProfiles)
-  app.get('/team/:teamId', getTeam)
-  app.get('/teams/:page', getTeams)
   app.get('/user_teams/:userId/:page', getUserTeams)
   app.get('/user_by_role/:rule/:page', getUsersByRules)
   app.patch('/update_user', updateUser)
   app.post('/sessions', authenticate)
+
   app.post('/teams', teams)
+  app.get('/team/:teamId', getTeam)
+  app.get('/teams/:page', getTeams)
   app.patch('/update_team', updateTeams)
 
   // Authenticated
