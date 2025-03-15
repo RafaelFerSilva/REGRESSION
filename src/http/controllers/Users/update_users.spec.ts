5a26c03094925f4788e6e811bc3bf8e5b5067b04
rafaelsilva <todosomtodavoz@gmail.com>
2025-03-15 11:13:30 -0300

    test: e2e - should return 400 when e-mail format is invalid

--- src/http/controllers/Users/update_users.spec.ts
+++ src/http/controllers/Users/update_users.spec.ts
@@ -99,4 +99,23 @@ describe('Update User (e2e)', () => {
     expect(response.statusCode).toEqual(400)
     expect(response.body.message).toEqual('Validation error')
   })
+
+  it('should return 400 when e-mail format is invalid', async () => {
+    const { token, user } = await createAndAuthenticateUser(app)
+    const newUser = makeUserData()
+
+    const response = await request(app.server)
+      .patch('/update_user')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        id: user.id,
+        name: newUser.name,
+        email: newUser.email,
+        password: '123',
+        rule: newUser.rule
+      })
+
+    expect(response.statusCode).toEqual(400)
+    expect(response.body.message).toEqual('Validation error')
+  })
 })
