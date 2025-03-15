a677145977d2b94e01c91bfae1e3dbb632f1ae67
rafaelsilva <todosomtodavoz@gmail.com>
2025-03-15 09:56:16 -0300

    test: e2e - should be return 401 when user not authenticated

--- src/http/controllers/Users/profile.spec.ts
+++ src/http/controllers/Users/profile.spec.ts
@@ -29,7 +29,7 @@ describe('Profile (e2e)', () => {
     )
   })
 
-  it('Should be return 404 when user not found', async () => {
+  it('should be return 404 when user not found', async () => {
     const token = app.jwt.sign({ sub: '1234567890' }, { expiresIn: '1d' });
     const profileResponse = await request(app.server)
       .get('/me')
@@ -38,5 +38,16 @@ describe('Profile (e2e)', () => {
 
     expect(profileResponse.statusCode).toEqual(404)
     expect(profileResponse.body.message).toEqual('User not found')
+  })
+
+  it('should be return 401 when user not authenticated', async () => {
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJqb2huQGV4YW1wbGUuY29tIiwicnVsZSI6IlFBIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE5MjA5ODg2MjJ9.MJN9SdQNFQGWJc_Yb-xHSXPWNm7I1KqXgT8-MxbVDp0'
+    const profileResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(401)
+    expect(profileResponse.body.message).toEqual('Unauthorized')
   })
 })
