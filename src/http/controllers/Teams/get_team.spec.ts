b366ef6beb0053c594c0af092e2350f51ad13813
rafaelsilva <todosomtodavoz@gmail.com>
2025-03-30 10:59:17 -0300

    test: should be able to get team by team Id

---
+++ src/http/controllers/Teams/get_team.spec.ts
@@ -0,0 +1,35 @@
+import { beforeAll, afterAll, describe, expect, it, beforeEach } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from 'lib/prisma'
+import { createTeam } from '@/use-cases/factories/User/create-team'
+
+describe('Get Team (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(async () => {
+    await prisma.team.deleteMany()
+    await prisma.user.deleteMany()
+  })
+
+  it('should be able to get team by team Id', async () => {
+    const admin = await createAndAuthenticateUser(app, 'ADMIN')
+    const team1 = await createTeam({ user: admin.user, token: admin.token })
+
+    const user = await createAndAuthenticateUser(app, 'QA')
+
+    const team = await request(app.server)
+      .get(`/team/${team1.team.id}`)
+      .set('Authorization', `Bearer ${user.token}`)
+
+    expect(team.statusCode).toEqual(200)
+    expect(team.body.team).toEqual(team1.team)
+  })
+})
